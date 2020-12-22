5a8851365f6db8124174b7b07cae547977730d8c
Lilith <lilith_niniane@outlook.com>
2020-12-21 23:34:42 -0600

    Correct repeated button creation to iteration

--- src/Components/ButtonPanel.js
+++ src/Components/ButtonPanel.js
@@ -1,38 +1,17 @@
 import React from 'react';
 import Button from './Button';
+import panelData from './Data/PanelData';
 
 export default function ButtonPanel() {
   return (
     <div className="button-panel">
-      <div className="row">
-        <Button name="all-clear" symbol="AC" />
-        <Button name="change-sign" symbol="+/-" />
-        <Button name="percentage" symbol="%" />
-        <Button name="division" symbol="÷" />
-      </div>
-      <div className="row">
-        <Button name="seven" symbol="7" />
-        <Button name="eight" symbol="8" />
-        <Button name="nine" symbol="9" />
-        <Button name="multiplication" symbol="x" />
-      </div>
-      <div className="row">
-        <Button name="four" symbol="4" />
-        <Button name="five" symbol="5" />
-        <Button name="six" symbol="6" />
-        <Button name="minus" symbol="-" />
-      </div>
-      <div className="row">
-        <Button name="one" symbol="1" />
-        <Button name="two" symbol="2" />
-        <Button name="three" symbol="3" />
-        <Button name="plus" symbol="+" />
-      </div>
-      <div className="row">
-        <Button name="zero" symbol="0" />
-        <Button name="decimal" symbol="." />
-        <Button name="equals" symbol="=" />
-      </div>
+      {panelData.map(panelData => (
+        <div key={panelData.id}>
+          {panelData.rowData.map(buttonData => (
+            <Button type="button" key={buttonData.id} name={buttonData.name} symbol={buttonData.symbol} />
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
