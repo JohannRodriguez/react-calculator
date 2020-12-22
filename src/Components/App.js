f7f7dd7c3ec8b247c897c683fca5e8890a266f10
Lilith <lilith_niniane@outlook.com>
2020-12-21 21:56:43 -0600

    Delete extra divs with the use of fragment

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import '../App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
 export default function App() {
   return (
-    <div className="App">
+    <>
       <Display />
       <ButtonPanel />
-    </div>
+    </>
   );
 }
