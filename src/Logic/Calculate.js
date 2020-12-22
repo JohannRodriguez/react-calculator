aab1f951ed9d941a51dbb62920df44d65b32e057
Lilith <lilith_niniane@outlook.com>
2020-12-22 11:21:28 -0600

    Create calculate function

--- src/Logic/Calculate.js
+++ src/Logic/Calculate.js
@@ -1,5 +1,26 @@
-const calculate = (data, name) => {
-  
-}
+const calculate = (data, buttonName) => {
+  let { total, next } = data;
+  const { operation } = data;
+  console.log(buttonName);
+  switch (buttonName) {
+    case '+':
+    case '-':
+    case 'x':
+    case '÷':
+      break;
+    case '+/-':
+      total *= -1;
+      next *= -1;
+      break;
+    case 'AC':
+      total = 0;
+      next = 0;
+      break;
+    default:
+      total = 'Error';
+      break;
+  }
+  return { total, next, operation };
+};
 
 export default calculate;
